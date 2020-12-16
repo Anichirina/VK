44c5a1d0f78fe782ba237e0d15a99f8ff5982e9d
Irina Anichkova <anichkova2016@yandex.ru>
2020-12-16 22:46:29 +0300

    created WallDelete.java

---
+++ src/main/java/ru/netology/domain/WallDelete.java
@@ -0,0 +1,12 @@
+package ru.netology.domain;
+
+public class WallDelete {
+    public void delete(int ownerId;int postId;) {
+        public String generateBlock() {
+            //TODO:add logic
+
+        }
+                       return;
+    }
+
+}
--- src/main/java/ru/netology/domain/WallSeachManager.java
+++ src/main/java/ru/netology/domain/WallSeachManager.java
@@ -2,7 +2,8 @@ package ru.netology.domain;
 
 public class WallSeachManager {
     private WallSeach[] wallSeaches;
-    public String generateBlock(int owerId, boolean domainInfo, boolean querlyInfo, int count, int offset, int extended )
+    public Post[] search(int ownerId, boolean domain, boolean querly, int oweOnly, int count, int offset, int extended)
+    public String generateBlock()
     {
         //TODO:add logic
         return null;
