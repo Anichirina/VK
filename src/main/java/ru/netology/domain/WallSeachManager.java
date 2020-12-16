58a0c281c5a44641a4734ea059de21de20dff056
Irina Anichkova <anichkova2016@yandex.ru>
2020-12-16 22:32:02 +0300

    created WallSeachManager.java

---
+++ src/main/java/ru/netology/domain/WallSeachManager.java
@@ -0,0 +1,10 @@
+package ru.netology.domain;
+
+public class WallSeachManager {
+    private WallSeach[] wallSeaches;
+    public String generateBlock(int owerId, boolean domainInfo, boolean querlyInfo, int count, int offset, int extended )
+    {
+        //TODO:add logic
+        return null;
+        }
+}
