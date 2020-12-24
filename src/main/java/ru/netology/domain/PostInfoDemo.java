0cde4fa1c9845c69e6c15542fd8ac6d801da52ce
Irina Anichkova <anichkova2016@yandex.ru>
2020-12-24 22:55:34 +0300

    add PostInfoDemo.java

---
+++ src/main/java/ru/netology/domain/PostInfoDemo.java
@@ -0,0 +1,130 @@
+package ru.netology.domain;
+
+public class PostInfoDemo {
+    String bookMarkInfo;
+    String logoInfo;
+    String nameInfo;
+    int dataInfo;
+    String textPostInfo;
+    int idUser;
+    int pictureIdPostInfo;
+    int likeInfo;
+    String repostInfo;
+    String commintInfo;
+    int viewPostInfo;
+    int favoritePageInfo;
+    int friendsIdCommint;
+    int friendsIdRepost;
+
+    public String getBookMarkInfo() {
+        return bookMarkInfo;
+    }
+
+    public void setBookMarkInfo(String bookMarkInfo) {
+        this.bookMarkInfo = bookMarkInfo;
+    }
+
+    public String getLogoInfo() {
+        return logoInfo;
+    }
+
+    public void setLogoInfo(String logoInfo) {
+        this.logoInfo = logoInfo;
+    }
+
+    public String getNameInfo() {
+        return nameInfo;
+    }
+
+    public void setNameInfo(String nameInfo) {
+        this.nameInfo = nameInfo;
+    }
+
+    public int getDataInfo() {
+        return dataInfo;
+    }
+
+    public void setDataInfo(int dataInfo) {
+        this.dataInfo = dataInfo;
+    }
+
+    public String getTextPostInfo() {
+        return textPostInfo;
+    }
+
+    public void setTextPostInfo(String textPostInfo) {
+        this.textPostInfo = textPostInfo;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public int getPictureIdPostInfo() {
+        return pictureIdPostInfo;
+    }
+
+    public void setPictureIdPostInfo(int pictureIdPostInfo) {
+        this.pictureIdPostInfo = pictureIdPostInfo;
+    }
+
+    public int getLikeInfo() {
+        return likeInfo;
+    }
+
+    public void setLikeInfo(int likeInfo) {
+        this.likeInfo = likeInfo;
+    }
+
+    public String getRepostInfo() {
+        return repostInfo;
+    }
+
+    public void setRepostInfo(String repostInfo) {
+        this.repostInfo = repostInfo;
+    }
+
+    public String getCommintInfo() {
+        return commintInfo;
+    }
+
+    public void setCommintInfo(String commintInfo) {
+        this.commintInfo = commintInfo;
+    }
+
+    public int getViewPostInfo() {
+        return viewPostInfo;
+    }
+
+    public void setViewPostInfo(int viewPostInfo) {
+        this.viewPostInfo = viewPostInfo;
+    }
+
+    public int getFavoritePageInfo() {
+        return favoritePageInfo;
+    }
+
+    public void setFavoritePageInfo(int favoritePageInfo) {
+        this.favoritePageInfo = favoritePageInfo;
+    }
+
+    public int getFriendsIdCommint() {
+        return friendsIdCommint;
+    }
+
+    public void setFriendsIdCommint(int friendsIdCommint) {
+        this.friendsIdCommint = friendsIdCommint;
+    }
+
+    public int getFriendsIdRepost() {
+        return friendsIdRepost;
+    }
+
+    public void setFriendsIdRepost(int friendsIdRepost) {
+        this.friendsIdRepost = friendsIdRepost;
+    }
+}
