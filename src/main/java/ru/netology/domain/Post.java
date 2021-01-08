7567588a5f209c5b290b20bd6f12716e506c008e
Ирина Аничкова <72977774+Anichirina@users.noreply.github.com>
2021-01-08 23:36:52 +0300

    Master (#3)
    * new pacadge menedger
    
    * new Post.java

--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -10,94 +10,30 @@ private String textInfo;
 private int replyOwnerId;
 private  int replyPostId;
 private int friendsOnly;
-private boolean commentsInfo;
-private boolean copyrightInfo;
-private boolean likeInfo;
-private boolean repostInfo;
-private  boolean viewInfo;
+private String commentsInfo;
+private String copyrightInfo;
+private String likeInfo;
+private String repostInfo;
+private String viewInfo;
 private String postTypeInfo;
-private boolean postSourceInfo;
-private boolean geoInfo;
-private  int singerIdInfo;
+private String postSourceInfo;
+private String geoInfo;
+private int singerIdInfo;
 private int canPinInfo;
 private int canDeleteInfo;
 private int canEditInfo;
 private int isPinnedInfo;
 private int markedAsAdsInfo;
-private boolean isFavoritInfo;
-private boolean donutInfo;
+private String isFavoritInfo;
+private String donutInfo;
 private int postponedIdInfo;
 
-    public boolean isViewInfo() {
-        return viewInfo;
+    public int getId() {
+        return id;
     }
 
-    public void setViewInfo(boolean viewInfo) {
-        this.viewInfo = viewInfo;
-    }
-
-    public String getTextInfo() {
-        return textInfo;
-    }
-
-    public void setTextInfo(String textInfo) {
-        this.textInfo = textInfo;
-    }
-
-    public int getSingerIdInfo() {
-        return singerIdInfo;
-    }
-
-    public void setSingerIdInfo(int singerIdInfo) {
-        this.singerIdInfo = singerIdInfo;
-    }
-
-    public boolean isRepostInfo() {
-        return repostInfo;
-    }
-
-    public void setRepostInfo(boolean repostInfo) {
-        this.repostInfo = repostInfo;
-    }
-
-    public int getReplyPostId() {
-        return replyPostId;
-    }
-
-    public void setReplyPostId(int replyPostId) {
-        this.replyPostId = replyPostId;
-    }
-
-    public int getReplyOwnerId() {
-        return replyOwnerId;
-    }
-
-    public void setReplyOwnerId(int replyOwnerId) {
-        this.replyOwnerId = replyOwnerId;
-    }
-
-    public String getPostTypeInfo() {
-        return postTypeInfo;
-    }
-
-    public void setPostTypeInfo(String postTypeInfo) {
-        this.postTypeInfo = postTypeInfo;
-    }
-
-    public boolean isPostSourceInfo() {
-        return postSourceInfo;
-    }
-
-    public void setPostSourceInfo(boolean postSourceInfo) {
-        this.postSourceInfo = postSourceInfo;
-    }
-
-    public int getPostponedIdInfo() {
-        return postponedIdInfo;
-    }
-
-    public void setPostponedIdInfo(int postponedIdInfo) {
-        this.postponedIdInfo = postponedIdInfo;
+    public void setId(int id) {
+        this.id = id;
     }
 
     public int getOwnerId() {
@@ -108,84 +44,12 @@ private int postponedIdInfo;
         this.ownerId = ownerId;
     }
 
-    public int getMarkedAsAdsInfo() {
-        return markedAsAdsInfo;
-    }
-
-    public void setMarkedAsAdsInfo(int markedAsAdsInfo) {
-        this.markedAsAdsInfo = markedAsAdsInfo;
-    }
-
-    public boolean isLikeInfo() {
-        return likeInfo;
-    }
-
-    public void setLikeInfo(boolean likeInfo) {
-        this.likeInfo = likeInfo;
-    }
-
-    public int getIsPinnedInfo() {
-        return isPinnedInfo;
-    }
-
-    public void setIsPinnedInfo(int isPinnedInfo) {
-        this.isPinnedInfo = isPinnedInfo;
-    }
-
-    public boolean isFavoritInfo() {
-        return isFavoritInfo;
-    }
-
-    public void setFavoritInfo(boolean favoritInfo) {
-        isFavoritInfo = favoritInfo;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public boolean isGeoInfo() {
-        return geoInfo;
-    }
-
-    public void setGeoInfo(boolean geoInfo) {
-        this.geoInfo = geoInfo;
-    }
-
     public int getFrontId() {
         return frontId;
     }
 
     public void setFrontId(int frontId) {
         this.frontId = frontId;
-    }
-
-    public int getFriendsOnly() {
-        return friendsOnly;
-    }
-
-    public void setFriendsOnly(int friendsOnly) {
-        this.friendsOnly = friendsOnly;
-    }
-
-    public boolean isDonutInfo() {
-        return donutInfo;
-    }
-
-    public void setDonutInfo(boolean donutInfo) {
-        this.donutInfo = donutInfo;
-    }
-
-    public int getDateInfo() {
-        return dateInfo;
-    }
-
-    public void setDateInfo(int dateInfo) {
-        this.dateInfo = dateInfo;
     }
 
     public int getCreatedId() {
@@ -196,20 +60,116 @@ private int postponedIdInfo;
         this.createdId = createdId;
     }
 
-    public boolean isCopyrightInfo() {
-        return copyrightInfo;
+    public int getDateInfo() {
+        return dateInfo;
     }
 
-    public void setCopyrightInfo(boolean copyrightInfo) {
-        this.copyrightInfo = copyrightInfo;
+    public void setDateInfo(int dateInfo) {
+        this.dateInfo = dateInfo;
     }
 
-    public boolean isCommentsInfo() {
+    public String getTextInfo() {
+        return textInfo;
+    }
+
+    public void setTextInfo(String textInfo) {
+        this.textInfo = textInfo;
+    }
+
+    public int getReplyOwnerId() {
+        return replyOwnerId;
+    }
+
+    public void setReplyOwnerId(int replyOwnerId) {
+        this.replyOwnerId = replyOwnerId;
+    }
+
+    public int getReplyPostId() {
+        return replyPostId;
+    }
+
+    public void setReplyPostId(int replyPostId) {
+        this.replyPostId = replyPostId;
+    }
+
+    public int getFriendsOnly() {
+        return friendsOnly;
+    }
+
+    public void setFriendsOnly(int friendsOnly) {
+        this.friendsOnly = friendsOnly;
+    }
+
+    public String getCommentsInfo() {
         return commentsInfo;
     }
 
-    public void setCommentsInfo(boolean commentsInfo) {
+    public void setCommentsInfo(String commentsInfo) {
         this.commentsInfo = commentsInfo;
+    }
+
+    public String getCopyrightInfo() {
+        return copyrightInfo;
+    }
+
+    public void setCopyrightInfo(String copyrightInfo) {
+        this.copyrightInfo = copyrightInfo;
+    }
+
+    public String getLikeInfo() {
+        return likeInfo;
+    }
+
+    public void setLikeInfo(String likeInfo) {
+        this.likeInfo = likeInfo;
+    }
+
+    public String getRepostInfo() {
+        return repostInfo;
+    }
+
+    public void setRepostInfo(String repostInfo) {
+        this.repostInfo = repostInfo;
+    }
+
+    public String getViewInfo() {
+        return viewInfo;
+    }
+
+    public void setViewInfo(String viewInfo) {
+        this.viewInfo = viewInfo;
+    }
+
+    public String getPostTypeInfo() {
+        return postTypeInfo;
+    }
+
+    public void setPostTypeInfo(String postTypeInfo) {
+        this.postTypeInfo = postTypeInfo;
+    }
+
+    public String getPostSourceInfo() {
+        return postSourceInfo;
+    }
+
+    public void setPostSourceInfo(String postSourceInfo) {
+        this.postSourceInfo = postSourceInfo;
+    }
+
+    public String getGeoInfo() {
+        return geoInfo;
+    }
+
+    public void setGeoInfo(String geoInfo) {
+        this.geoInfo = geoInfo;
+    }
+
+    public int getSingerIdInfo() {
+        return singerIdInfo;
+    }
+
+    public void setSingerIdInfo(int singerIdInfo) {
+        this.singerIdInfo = singerIdInfo;
     }
 
     public int getCanPinInfo() {
@@ -220,6 +180,14 @@ private int postponedIdInfo;
         this.canPinInfo = canPinInfo;
     }
 
+    public int getCanDeleteInfo() {
+        return canDeleteInfo;
+    }
+
+    public void setCanDeleteInfo(int canDeleteInfo) {
+        this.canDeleteInfo = canDeleteInfo;
+    }
+
     public int getCanEditInfo() {
         return canEditInfo;
     }
@@ -228,11 +196,43 @@ private int postponedIdInfo;
         this.canEditInfo = canEditInfo;
     }
 
-    public int getCanDeleteInfo() {
-        return canDeleteInfo;
+    public int getIsPinnedInfo() {
+        return isPinnedInfo;
     }
 
-    public void setCanDeleteInfo(int canDeleteInfo) {
-        this.canDeleteInfo = canDeleteInfo;
+    public void setIsPinnedInfo(int isPinnedInfo) {
+        this.isPinnedInfo = isPinnedInfo;
+    }
+
+    public int getMarkedAsAdsInfo() {
+        return markedAsAdsInfo;
+    }
+
+    public void setMarkedAsAdsInfo(int markedAsAdsInfo) {
+        this.markedAsAdsInfo = markedAsAdsInfo;
+    }
+
+    public String getIsFavoritInfo() {
+        return isFavoritInfo;
+    }
+
+    public void setIsFavoritInfo(String isFavoritInfo) {
+        this.isFavoritInfo = isFavoritInfo;
+    }
+
+    public String getDonutInfo() {
+        return donutInfo;
+    }
+
+    public void setDonutInfo(String donutInfo) {
+        this.donutInfo = donutInfo;
+    }
+
+    public int getPostponedIdInfo() {
+        return postponedIdInfo;
+    }
+
+    public void setPostponedIdInfo(int postponedIdInfo) {
+        this.postponedIdInfo = postponedIdInfo;
     }
 }
