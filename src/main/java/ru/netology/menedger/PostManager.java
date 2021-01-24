78d111f3849f876dc7b76b9b016d196e29932ee7
Irina Anichkova <anichkova2016@yandex.ru>
2021-01-24 19:54:54 +0300

    change Post.java

--- src/main/java/ru/netology/menedger/PostManager.java
+++ src/main/java/ru/netology/menedger/PostManager.java
@@ -3,7 +3,9 @@ package ru.netology.menedger;
 public class PostManager {
     private Post[] post;
 
-    public Post[] searchPost(int ownerId, String domain, String query, int ownersOnly, int count, int offset)
+    public Post[] searchPost(int ownerId, String domain, String query, int ownersOnly, int count, int offset){
+        return null;
+    }
 
     public String generateBlock() {
         //TODO:add logic
